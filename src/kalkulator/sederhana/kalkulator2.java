04ead2fb06eb8d2e44cd9b2f2f0ecb2a918cb34a
celcimonica <chelsimonica19@gmail.com>
2025-02-06 20:55:13 +0700

    buat kalkulator java

--- src/kalkulator/sederhana/kalkulator2.java
+++ src/kalkulator/sederhana/kalkulator2.java
@@ -52,7 +52,7 @@ public class kalkulator2 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(153, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(255, 204, 204));
 
         tampilText.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         tampilText.addActionListener(new java.awt.event.ActionListener() {
